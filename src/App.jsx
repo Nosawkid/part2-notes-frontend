e411c67f278e4cfe2cf11018b9ab98206312e184
Muhammed Yaseen Sidhik <mhdyaseensid@gmail.com>
2024-10-26 21:37:48 +0530

    Initisl commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,99 @@
-import Note from './components/Note'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Note from "./components/Note";
+import noteServices from "./services/notes";
+import Notification from "./components/Notification";
+import Footer from "./components/Footer";
 
-const App = ({ notes }) => {
+const App = () => {
+  const [notes, setNotes] = useState(null);
+  const [newNote, setNewNote] = useState("A new note");
+  const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const addNote = (e) => {
+    e.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+    };
+
+    noteServices.createItem(noteObject).then((res) => {
+      setNotes(notes.concat(res));
+    });
+
+    setNewNote("");
+  };
+
+  const handleNoteChange = (e) => {
+    console.log(e.target.value);
+    setNewNote(e.target.value);
+  };
+
+  const toggleImportance = (id) => {
+    const url = `http://localhost:3001/notes/${id}`;
+    const note = notes.find((n) => n.id === id);
+    const changedNote = {
+      ...note,
+      important: !note.important,
+    };
+
+    noteServices
+      .updateItem(id, changedNote)
+      .then((res) => {
+        setNotes(notes.map((note) => (note.id === id ? res : note)));
+      })
+      .catch((error) => {
+        setErrorMessage(`The not ${note.content} was removed from the server`);
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
+        setNotes(notes.filter((n) => n.id !== id``));
+      });
+  };
+
+  const notesToShow = showAll ? notes : notes.filter((el) => el.important);
+
+  useEffect(() => {
+    noteServices
+      .getAll()
+      .then((res) => {
+        setNotes(res);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  if (!notes) {
+    return null;
+  }
+
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
-        {notes.map(note => 
-          <Note key={note.id} note={note} />
-        )}
+        {notesToShow.map((note) => (
+          <Note
+            key={note.id}
+            note={note}
+            toggleImportance={() => toggleImportance(note.id)}
+          />
+        ))}
       </ul>
+      <form onSubmit={addNote}>
+        <input onChange={handleNoteChange} type="text" value={newNote} />
+        <button type="submit">Save</button>
+      </form>
+      <Footer />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ src/services/notes.js
@@ -0,0 +1,26 @@
+import axios from "axios";
+const baseUrl = "http://localhost:3001/api/notes";
+
+const getAll = () => {
+  const request = axios.get(baseUrl);
+
+  return request
+    .then((response) => response.data)
+    .catch((err) => {
+      console.log(err.message);
+    });
+};
+
+const createItem = (newItem) => {
+  return axios.post(baseUrl, newItem).then((res) => res.data);
+};
+
+const updateItem = (id, newObj) => {
+  return axios.put(`${baseUrl}/${id}`, newObj).then((res) => res.data);
+};
+
+export default {
+  getAll,
+  createItem,
+  updateItem,
+};
